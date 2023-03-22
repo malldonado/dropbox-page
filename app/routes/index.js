810e719dd4f097c0b54dfbee1cff7ad897232f0c
Matheus Silva <47877623+matheusmaldonadosilva@users.noreply.github.com>
2023-03-22 07:12:43 -0300

    update

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,8 +4,38 @@ var formidable = require('formidable');
 var fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+router.get('/file', (req, res) => {
+
+  let path = './' + req.query.path;
+
+
+  if(fs.existsSync(path)) {
+    fs.readFile(path, (err, data) => {
+      if(err) {
+        console.log(err);
+        res.status(400).json({
+          error: err
+        })
+      }
+      else {
+        res.status(200).end(data);
+      }
+    })
+  }
+
+  else {
+
+    res.status(404).json({
+      error: 'File not found'
+    })
+  }
+
 });
 
 router.post('/upload', (req, res) => {
@@ -19,20 +49,23 @@ router.post('/upload', (req, res) => {
 
     let path = './' + fields.path;
 
-    if(fs.existSync(path)) {
-      fs.unlink(path, err=> {
-        fs.unlink(path, err => {
-          if(err) {
-            res.status(400).json({
-              err
-            });
-            } else {
-              res.json({
-                fields
-              });
-            }
-        });
+    if (fs.existSync(path)) {
+      fs.unlink(path, err => {
+        if (err) {
+          res.status(400).json({
+            err
+          });
+        } else {
+          res.json({
+            fields
+          });
+        }
       });
+    }
+    else {
+      res.status(404).json({
+        error: 'File not found'
+      })
     }
 
     res.json({
@@ -50,7 +83,7 @@ router.delete('/file', (req, res) => {
 
   form.parse(req, (err, fields, files) => {
 
-    
+
 
     res.json({
       files
